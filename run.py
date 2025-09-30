a9795773a02f1867e48d4ad079e126a25dea2d11
Alexandre-Jasmin <146493685+Alexandre-Jasmin@users.noreply.github.com>
2025-09-30 19:45:00 -0400

    1.4
    latest build

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_flask_app
 from app.services.ddragon_updater import DataDragonUpdater
 
+# before creating application, make sure assets are up to date
 ddragon_updater = DataDragonUpdater()
 current_version = ddragon_updater.update_league_assets()
 
